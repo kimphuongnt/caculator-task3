20abf7c3281a2aebbfae25163e465c4fb2747e6f
kimphuongnt <kimphuongnt.dev@gmail.com>
2024-08-16 17:15:20 +0700

    task3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useMemo, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
+  const [result, setResult] = useState(0);
+  const [resultMemo, setResultMemo] = useState(0);
+  const [operation, setOperation] = useState("+");
+
+  function Calculator(a, b, opera) {
+    const numa = parseInt(a), numb = parseInt(b);
+    switch (opera) {
+      case "+":
+        return numa + numb;
+      case "-":
+        return numa - numb;
+      case "*":
+        return numa * numb;
+      case "/":
+        return numb !== 0 ? numa / numb : "Error";
+      default:
+        return 0;
+    }
+  }
+
+  const handleMemo = useMemo(() => {
+    console.time("Time Calculator Memo");
+    const calMemo = Calculator(num1, num2, operation);
+    console.timeEnd("Time Calculator Memo");
+    setResultMemo(calMemo);
+  }, [num1, num2, operation]);
+
+  const handle = () => {
+    console.time("Time Calculator");
+    const cal = Calculator(num1, num2, operation);
+    console.timeEnd("Time Calculator");
+    setResult(cal);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input
+        type="number"
+        value={num1}
+        onChange={(e) => setNum1(e.target.value)}
+      />
+      <select value={operation} onChange={(e) => setOperation(e.target.value)}>
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value="*">*</option>
+        <option value="/">/</option>
+      </select>
+      <input
+        type="number"
+        value={num2}
+        onChange={(e) => setNum2(e.target.value)}
+      />
+      <button onClick={handleMemo}>Calculate Memo</button>
+
+      <button onClick={handle}>Calculate</button>
+      <div>ResultMemo: {resultMemo}</div>
+      <div>Result: {result}</div>
+    </>
   );
 }
 
